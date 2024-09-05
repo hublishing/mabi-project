69d377f0908c001ee672b05d6dc7b81ed13f7e23
han <jaehun_@kakao.com>
2024-09-05 16:01:45 +0900

    push

--- public/js/join.js
+++ public/js/join.js
@@ -1,1 +1,21 @@
-console.log("Join JS 로드됨");
+import { auth } from '../firebase.js';
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+
+document.querySelector('form').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    
+    const email = document.querySelector('input[type="text"]').value;
+    const password = document.querySelector('input[type="password"]').value;
+
+    try {
+        // Firebase에서 사용자 생성
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+        const user = userCredential.user;
+        console.log("회원가입 성공:", user);
+
+        // 회원가입 성공 후 로그인 페이지로 리디렉션
+        window.location.href = "../index.html"; // 로그인 페이지로 이동
+    } catch (error) {
+        console.error("회원가입 실패:", error.message);
+    }
+});
